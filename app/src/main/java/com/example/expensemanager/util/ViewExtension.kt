0fc2577750e718718ebcecc41162332f06fd0e88
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:47:37 +0900

    adding

--- app/src/main/java/com/example/expensemanager/util/ViewExtension.kt
+++ app/src/main/java/com/example/expensemanager/util/ViewExtension.kt
@@ -1,6 +1,12 @@
 package com.example.expensemanager.util
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun View.show() {
     visibility = View.VISIBLE
@@ -9,3 +15,47 @@ fun View.show() {
 fun View.hide() {
     visibility = View.GONE
 }
+fun convertToGlobal (amount: Double): String {
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.maximumFractionDigits = 0
+    format.currency = Currency.getInstance("USD")
+    return format.format(amount)
+}
+fun parseDouble(value: String?): Double {
+    return if (value == null || value.isEmpty()) Double.NaN else value.toDouble()
+}
+
+fun TextInputEditText.modifyIntoDatePicker(
+    context: Context,
+    format: String,
+    maxDate: Date? = null
+) {
+    isFocusableInTouchMode = false
+    isClickable = true
+    isFocusable = false
+
+    val myCalendar = Calendar.getInstance()
+    val datePickerOnDataSetListener =
+        DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, monthOfYear)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            val sdf = SimpleDateFormat(format, Locale.UK)
+            setText(sdf.format(myCalendar.time))
+        }
+
+    setOnClickListener {
+        DatePickerDialog(
+            context,
+            datePickerOnDataSetListener,
+            myCalendar
+                .get(Calendar.YEAR),
+            myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)
+        ).run {
+            maxDate?.time?.also { datePicker.maxDate = it }
+            show()
+        }
+    }
+}
+
