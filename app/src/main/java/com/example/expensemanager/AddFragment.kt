006093cf353f2a12227e4d47f86882ee90745a6b
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:47:15 +0900

    adding

--- app/src/main/java/com/example/expensemanager/AddFragment.kt
+++ app/src/main/java/com/example/expensemanager/AddFragment.kt
@@ -1,28 +1,105 @@
 package com.example.expensemanager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.expensemanager.base.BaseFragment
+import com.example.expensemanager.base.BaseViewModel
+import com.example.expensemanager.databinding.FragmentAddBinding
+import com.example.expensemanager.model.Expense
+import com.example.expensemanager.util.Constants
+import com.example.expensemanager.util.modifyIntoDatePicker
+import com.example.expensemanager.util.parseDouble
+import java.util.*
 
 
-class AddFragment : Fragment() {
+class AddFragment : BaseFragment<FragmentAddBinding,BaseViewModel>() {
 
+    override val viewModel: BaseViewModel by activityViewModels()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeView()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+    private fun initializeView() {
+
+        val expenseTypeAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.item_autocomplete_layout,
+            Constants.expenseType
+        )
+
+        val expenseTagAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.item_autocomplete_layout,
+            Constants.expenseTags
+        )
+
+        with(binding) {
+            addLayout.addType.setAdapter(expenseTypeAdapter)
+            addLayout.addTag.setAdapter(expenseTagAdapter)
+
+            addLayout.addDate.modifyIntoDatePicker(
+                requireContext(),
+                "dd/MM/yyyy",
+                 Date()
+            )
+            buttonSave.setOnClickListener {
+                binding.addLayout.apply {
+                val(title,amount,type,tag,date,desc) = getExpenseBinding()
+                    when {
+                        title.isEmpty() -> {
+                            this.addTitle.error = "The title must not be empty"
+                        }
+                        amount.isNaN() -> {
+                            this.addAmount.error = "This amount must not be empty"
+                        }
+                        type.isEmpty() -> {
+                            this.addType.error = "This type must not be empty"
+                        }
+                        tag.isEmpty() -> {
+                            this.addTag.error = "This tag must not be empty."
+                        }
+                        date.isEmpty() -> {
+                            this.addDate.error = "This date must not be empty"
+                        }
+                        desc.isEmpty() -> {
+                            this.addDesc.error = "This description must note be empty."
+                        }
+                        else -> {
+                            viewModel.insertExpense(getExpenseBinding()).run {
+                                toast(getString(R.string.successfully_save))
+                                findNavController().navigate(R.id.action_addFragment_to_mainFragment)
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
     }
+
+    private fun getExpenseBinding() : Expense = binding.addLayout.let {
+        val title = it.addTitle.text.toString()
+        val amount = parseDouble(it.addAmount.text.toString())
+        val type = it.addType.text.toString()
+        val tag = it.addTag.text.toString()
+        val date = it.addDate.text.toString()
+        val desc = it.addDesc.text.toString()
+
+        return Expense(title,amount,type,tag,date,desc)
+    }
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentAddBinding.inflate(inflater,container,false)
 
 
 }
