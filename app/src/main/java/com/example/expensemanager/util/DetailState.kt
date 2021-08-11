6eb67ab11d4b211117d0dcb57f8373ac0260033f
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:46:43 +0900

    adding

---
+++ app/src/main/java/com/example/expensemanager/util/DetailState.kt
@@ -0,0 +1,10 @@
+package com.example.expensemanager.util
+
+import com.example.expensemanager.model.Expense
+
+sealed class DetailState {
+    object Loading : DetailState()
+    object Empty : DetailState()
+    data class Success(val expense: Expense) : DetailState()
+    data class Error(val exception: Throwable) : DetailState()
+}
