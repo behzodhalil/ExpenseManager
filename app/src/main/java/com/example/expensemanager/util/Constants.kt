c386fee25c06f0b49fc063e7555f1d7022a7df01
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:46:40 +0900

    adding

---
+++ app/src/main/java/com/example/expensemanager/util/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.expensemanager.util
+
+object Constants {
+
+    val expenseType = listOf("Income", "Expense")
+
+    val expenseTags = listOf(
+        "Housing",
+        "Transportation",
+        "Food",
+        "Utilities",
+        "Insurance",
+        "Healthcare",
+        "Saving & Debts",
+        "Personal Spending",
+        "Entertainment",
+        "Miscellaneous"
+    )
+}
