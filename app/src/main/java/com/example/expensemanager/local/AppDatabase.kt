64d1be30c340676b9b94994dc5ae6d6231aa1e92
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:47:19 +0900

    adding

---
+++ app/src/main/java/com/example/expensemanager/local/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.expensemanager.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.expensemanager.model.Expense
+
+@Database(
+    entities = [Expense::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase()
+{
+    abstract fun getExpenseDao(): ExpenseDao
+
+    companion object {
+
+        @Volatile
+        private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        // Check for DB instance if not null then get or insert or else create new DB Instance
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "expense.db"
+        ).build()
+    }
+}
