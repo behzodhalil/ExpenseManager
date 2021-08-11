6be03300be552d979bc7af464a81d393fc67806f
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-12 00:47:31 +0900

    adding

---
+++ app/src/main/java/com/example/expensemanager/util/ViewState.kt
@@ -0,0 +1,10 @@
+package com.example.expensemanager.util
+
+import com.example.expensemanager.model.Expense
+
+sealed class ViewState {
+    object Loading : ViewState()
+    object Empty : ViewState()
+    data class Success(val expense: List<Expense>) : ViewState()
+    data class Error(val exception: Throwable) : ViewState()
+}
