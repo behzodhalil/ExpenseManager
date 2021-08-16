cb39b629676c2417ceb145e427c60e931a0ebccb
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-16 15:38:08 +0900

    Updating with "Fragments"

--- app/src/main/java/com/example/expensemanager/DetailsFragment.kt
+++ app/src/main/java/com/example/expensemanager/DetailsFragment.kt
@@ -5,22 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.example.expensemanager.base.BaseFragment
+import com.example.expensemanager.base.BaseViewModel
+import com.example.expensemanager.databinding.FragmentDetailsBinding
+import com.example.expensemanager.model.Expense
+import com.example.expensemanager.util.DetailState
+import com.example.expensemanager.util.convertToGlobal
+import kotlinx.coroutines.flow.collect
+import kotlin.math.exp
 
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : BaseFragment<FragmentDetailsBinding,BaseViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+    private val args: DetailsFragmentArgs by navArgs()
+    override val viewModel: BaseViewModel by activityViewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val expense = args.expense
+        getById(expense.id)
+        observeItems()
     }
+
+    private fun getById(id: Int) {
+        viewModel.getById(id)
+    }
+
+    private fun observeItems() = lifecycleScope.launchWhenCreated {
+
+        viewModel.detailState.collect() {detailState ->
+
+            when(detailState){
+                DetailState.Loading -> {
+
+                }
+                is DetailState.Error -> {
+                    toast("Error!")
+                }
+                is DetailState.Success -> {
+                    onDetailsSaved(detailState.expense)
+                }
+            }
+
+        }
+    }
+
+    private fun onDetailsSaved(expense: Expense) = with(binding.expenseDetailsLayout) {
+        detailsTitle.text = expense.title
+        detailsAmount.text = convertToGlobal(expense.amount)
+        detailsType.text = expense.type
+        detailsTag.text = expense.tag
+        detailsDate.text = expense.date
+        detailsDesc.text = expense.note
+
+    }
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) =  FragmentDetailsBinding.inflate(inflater,container,false)
+
 
 
 }
--- app/src/main/java/com/example/expensemanager/MainFragment.kt
+++ app/src/main/java/com/example/expensemanager/MainFragment.kt
@@ -1,45 +1,128 @@
 package com.example.expensemanager
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.expensemanager.base.BaseFragment
+import com.example.expensemanager.base.BaseViewModel
 import com.example.expensemanager.databinding.FragmentMainBinding
+import com.example.expensemanager.model.Expense
+import com.example.expensemanager.util.*
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 
-class MainFragment : Fragment() {
+import kotlin.math.abs
 
-    private var mainBinding : FragmentMainBinding? = null
-    private val binding get() = mainBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+@AndroidEntryPoint
+class MainFragment : BaseFragment<FragmentMainBinding,BaseViewModel>() {
+
+
+    private lateinit var expenseAdapter: ExpenseAdapter
+    override val viewModel: BaseViewModel by activityViewModels()
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?) = FragmentMainBinding.inflate(inflater, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+        initializeView()
+        observeExpense()
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-
-        mainBinding = FragmentMainBinding.inflate(inflater,container,false)
-
-
-
-        navigateAdd()
-
-        return binding.root
-
-
+    //function
+    private fun setUpRecyclerView() = with(binding) {
+        expenseAdapter = ExpenseAdapter()
+        val recyclerView = binding.controlRecylerView
+        recyclerView.adapter = expenseAdapter
+        recyclerView.layoutManager = LinearLayoutManager(activity)
     }
 
-    private fun navigateAdd() {
-        binding.btnAddTransaction.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_addFragment)
+    // function
+    private fun onTotalExpenseLoading(expense: List<Expense>) = with(binding) {
+        val (totalIncome, totalExpense) = expense.partition { it.type == "Income" }
+        val income = totalIncome.sumOf { it.amount }
+        val expense = totalExpense.sumOf{ it.amount }
+        incomeAmount.text = "+ ".plus(convertToGlobal(income))
+        expenseAmount.text = "- ".plus(convertToGlobal(expense))
+        balanceAmount.text = convertToGlobal(income - expense)
+    }
+
+
+
+    //function
+
+    private fun observeExpense() = lifecycleScope.launchWhenCreated {
+        viewModel.viewState.collect { viewState ->
+            when(viewState){
+                is ViewState.Loading -> {
+
+                }
+                is ViewState.Success -> {
+                    showAllViews()
+                    onExpenseLoaded(viewState.expense)
+                    onTotalExpenseLoading(viewState.expense)
+                }
+                is ViewState.Empty ->{
+                    hideAllViews()
+                }
+                is ViewState.Error -> {
+                    toast("Error")
+                }
+            }
         }
     }
+
+    private fun showAllViews() = with(binding) {
+        mainRelative.show()
+        emptyLayout.hide()
+        controlRecylerView.show()
+    }
+
+    private fun hideAllViews() = with(binding) {
+        mainRelative.hide()
+        emptyLayout.show()
+    }
+
+    //function
+    private fun onExpenseLoaded(list: List<Expense>)  = expenseAdapter.differ.submitList(list)
+
+    //function
+    private fun initializeView() = with(binding) {
+        btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_addFragment)
+        }
+        mainDashboardScrollView.setOnScrollChangeListener(
+            NestedScrollView.OnScrollChangeListener { _, sX, sY, oX, oY ->
+                if (abs(sY - oY) > 10) {
+                    when {
+                        sY > oY -> btnAdd.hide()
+                        oY > sY -> btnAdd.show()
+                    }
+                }
+            }
+        )
+        expenseAdapter.setOnClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("expense",it)
+            }
+            findNavController().navigate(R.id.action_mainFragment_to_detailsFragment)
+            bundle
+        }
+    }
+
 
 
 }
