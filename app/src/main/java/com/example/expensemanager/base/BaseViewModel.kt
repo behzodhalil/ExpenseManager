23ee682de72ec4200d728034b7a04215d406cac8
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-16 15:38:44 +0900

    Updating with "ViewModel"

--- app/src/main/java/com/example/expensemanager/base/BaseViewModel.kt
+++ app/src/main/java/com/example/expensemanager/base/BaseViewModel.kt
@@ -2,17 +2,22 @@ package com.example.expensemanager.base
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import com.example.expensemanager.model.Expense
 import com.example.expensemanager.repo.ExpenseRepository
 import com.example.expensemanager.util.DetailState
 import com.example.expensemanager.util.ViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BaseViewModel constructor(
+@HiltViewModel
+class BaseViewModel @Inject constructor(
     application: Application,
     private val repo: ExpenseRepository
 ) : AndroidViewModel(application)
@@ -20,8 +25,9 @@ class BaseViewModel constructor(
     private val _viewState = MutableStateFlow<ViewState>(ViewState.Loading)
     private val _detailState = MutableStateFlow<DetailState>(DetailState.Loading)
 
-    private val viewState: StateFlow<ViewState> = _viewState
-    private val detailState : StateFlow<DetailState> = _detailState
+
+    val viewState: StateFlow<ViewState> = _viewState
+    val detailState : StateFlow<DetailState> = _detailState
 
 
     fun insertExpense(expense: Expense) = viewModelScope.launch {
@@ -40,11 +46,24 @@ class BaseViewModel constructor(
         repo.deleteById(id)
     }
 
+
     fun getById(id: Int) = viewModelScope.launch {
         _detailState.value = DetailState.Loading
         repo.getById(id).collect { result: Expense? ->
             if (result != null) {
                 _detailState.value = DetailState.Success(result)
+            }
+        }
+    }
+    init {
+        viewModelScope.launch {
+            repo.getAllItem().collect { result ->
+                if(result.isNullOrEmpty()) {
+                    _viewState.value = ViewState.Empty
+                }
+                else {
+                    _viewState.value = ViewState.Success(result)
+                }
             }
         }
     }
