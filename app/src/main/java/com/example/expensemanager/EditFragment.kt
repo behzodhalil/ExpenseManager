931d35d5b83c8aa0c94e5205cd629e0906f4d14d
Behzod Bozorboev <behzoddev@gmail.com>
2021-08-22 15:26:12 +0900

    updating

--- app/src/main/java/com/example/expensemanager/EditFragment.kt
+++ app/src/main/java/com/example/expensemanager/EditFragment.kt
@@ -9,11 +9,6 @@ import android.view.ViewGroup
 
 class EditFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
